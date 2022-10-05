b29e680831b70f247614359c46d51e90a968ea72
nno3onn <nno3onn@naver.com>
2022-10-05 21:59:20 +0900

    nextjs vs reactjs

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
 export default function Potato() {
+  // component(혹은 함수) 이름은 중요치 않음. export default가 중요함.
   return "about us";
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
 export default function Home() {
-  return "hi";
+  // 1. 모든 파일에 React를 import할 필요 없음(hooks를 사용하지 않는 이상)
+  // 2. 404 페이지를 만들 필요 없음
+  // 3. 파일 확장명을 jsx로 지을 필요 없음
+
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
 }
