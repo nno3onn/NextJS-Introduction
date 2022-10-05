29774041ad5325345fe4c2bc8beed6ade3b6eb16
nno3onn <nno3onn@naver.com>
2022-10-05 22:21:31 +0900

    study csr & ssr

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+// 1. 모든 파일에 React를 import할 필요 없음(hooks를 사용하지 않는 이상)
+// 2. 404 페이지를 만들 필요 없음
+// 3. 파일 확장명을 jsx로 지을 필요 없음
+
+import { useState } from "react";
+
 export default function Home() {
-  // 1. 모든 파일에 React를 import할 필요 없음(hooks를 사용하지 않는 이상)
-  // 2. 404 페이지를 만들 필요 없음
-  // 3. 파일 확장명을 jsx로 지을 필요 없음
+  const [counter, setCounter] = useState(0);
 
   return (
     <div>
-      <h1>hello</h1>
+      <h1>hello {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
     </div>
   );
 }
