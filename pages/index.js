c5a7204c968b58f4b766dbf8284650cd2745d0f7
nno3onn <nno3onn@naver.com>
2022-10-06 14:55:45 +0900

    routing method 1)

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@
 // 2. 404 페이지를 만들 필요 없음
 // 3. 파일 확장명을 jsx로 지을 필요 없음
 
+import { useRouter } from "next/router";
+import Link from "next/link";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
@@ -12,7 +14,11 @@ export default function Home({ results }) {
       {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <h4>
+            <Link href={`/movies/${movie.id}`}>
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
