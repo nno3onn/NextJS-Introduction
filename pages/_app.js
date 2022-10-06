640c6cf961bbca1c1595e2509d0cec057d58a766
nno3onn <nno3onn@naver.com>
2022-10-06 14:35:13 +0900

    SSR(Server SIde Rendering)

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
-// _app 파일에 적은 코드 넣기(Layout Component 활용)
-
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
@@ -10,3 +8,9 @@ export default function App({ Component, pageProps }) {
     </Layout>
   );
 }
+
+// 1. nextjs가 home을 받음
+// 2. getServerSideProps 를 사용할 걸 알고 API를 호출
+// 3. props를 return함
+// 4. nextjs가 APp의 pageProps부분에 3의 props를 넣어 보내줌
+// 5. home에 props를 받고 해당 데이터를 띄움
--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,14 @@
 // 2. 404 페이지를 만들 필요 없음
 // 3. 파일 확장명을 jsx로 지을 필요 없음
 
-import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "4b9deaf6bccf0ef59e596f59eba8a566";
-
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    // IIFE(즉시실행함수표현. Immediately Invoked Function Expression)
-    (async () => {
-      const { results } = await (await fetch("/api/movies")).json();
-      setMovies(results);
-    })();
-  }, []);
-
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading ...</h4>}
-      {movies.map((movie) => (
+      {!results && <h4>Loading...</h4>}
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -51,4 +38,27 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+// ! getServerSideProps 이름 꼭 지킬 것!
+// client에 보여주지 않는 부분.
+// 코드는 서버로 돌아감. server쪽에서만 작동함. 그걸 이용해서 API key도 숨길 수 있다!
+// return 값은 위의 export default function에 props로 전달함
+// await fetch("/api/movies")의 fetch url은 front에서 작동하지만 서버에서는 작동하지 않는다.
+// 프론트엔드에는 이미 브라우저에 URL에 있어서 된다. 하지만 서버에는 없으니까 http://localhost:3000을 추가해주어야 한다.
+// 이제 소스코드 안에 html이 모두 들어간다. 그래서 loading할 필요가 없다. API가 돌아오기 전까지 화면에 아무것도 안 보일거다.
+
+// 1. 데이터를 모두 가져온 뒤 띄워서 소스코드에 모든 정보를 보여주거나, 2. 혹은 데이터를 가져오기 전까지 loading을 띄워서 소스코드에 loading만 띄워줄 것인지 선택해야 한다.
+// 1의 경우, nextJS가 백엔드에서 받아온 props를 return해서 여기에 가져다 주면, reactJS가 props를 가져와서 그걸 가지고 result Array를 뽑아준다!
+// page가 유저에게 보여지기 전에 props를 받아오는 function을 만들어야 한다.
+// 하지만 1은 유저가 아무것도 보지 못한 채로 오래 기다려야 한다는 단점이 있다.
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch("http://localhost:3000/api/movies")
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
