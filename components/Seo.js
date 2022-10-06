35828f54a55b7e8591afd6a56e7fb69d9a3da2e7
nno3onn <nno3onn@naver.com>
2022-10-06 12:37:26 +0900

    patterns (layout, seo - Head & title)

--- components/Seo.js
+++ components/Seo.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+// react였다면 react-helmet 사용해야 했을 것
 
 export default function Seo({ title }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@
 // 2. 404 페이지를 만들 필요 없음
 // 3. 파일 확장명을 jsx로 지을 필요 없음
 
-// react였다면 react-helmet 사용해야 했을 것
-import Head from "next/head";
 import Seo from "../components/Seo";
 
 export default function Home() {
